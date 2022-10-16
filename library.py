78d57219390a3170e4f299b14dfa116c9d0f0c5e
Aliza Lisan <36656209+alizalisan@users.noreply.github.com>
2022-10-16 11:59:17 -0700

    Add three transformers from chapter 2

--- library.py
+++ library.py
@@ -2,3 +2,101 @@ import pandas as pd
 import numpy as np
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.pipeline import Pipeline
+
+
+#This class will rename one or more columns.
+class RenamingTransformer(BaseEstimator, TransformerMixin):
+
+  def __init__(self, mapping_dict:dict):
+    assert isinstance(mapping_dict, dict), f'{self.__class__.__name__} constructor expected dictionary but got {type(mapping_dict)} instead.'
+    self.mapping_dict = mapping_dict
+
+  def fit(self, X, y = None):
+    print(f"\nWarning: {self.__class__.__name__}.fit does nothing.\n")
+    return X
+
+  def transform(self, X):
+    assert isinstance(X, pd.core.frame.DataFrame), f'{self.__class__.__name__}.transform expected Dataframe but got {type(X)} instead.'
+
+    #now check to see if some column names in the mapping dict are not in the dataframe
+    keys = set(self.mapping_dict.keys())
+    cols = set(X.columns)
+
+    diff = keys - cols
+    
+    assert len(diff) == 0, f'{self.__class__.__name__}. Unmatched column(s) "{diff}"'
+
+    X_ = X.copy()
+    X_.rename(columns=self.mapping_dict, inplace=True)
+    return X_
+
+  def fit_transform(self, X, y = None):
+    result = self.transform(X)
+    return result
+
+class OHETransformer(BaseEstimator, TransformerMixin):
+  def __init__(self, target_column, dummy_na=False, drop_first=False):  
+    assert isinstance(target_column, str), f'{self.__class__.__name__} constructor expected string but got {type(target_column)} instead.'
+    self.target_column = target_column
+    self.dummy_na = dummy_na
+    self.drop_first = drop_first
+
+  def fit(self, X, y = None):
+    print(f"\nWarning: {self.__class__.__name__}.fit does nothing.\n")
+    return X
+
+  def transform(self, X):
+    assert isinstance(X, pd.core.frame.DataFrame), f'{self.__class__.__name__}.transform expected Dataframe but got {type(X)} instead.'
+    target_column = set([self.target_column])
+    cols = set(X.columns)
+    diff = target_column - cols
+
+    assert len(diff) == 0 , f'{self.__class__.__name__}. Unmatched column(s) "{diff}"'
+    X_ = X.copy()
+    X_ = pd.get_dummies(X_,
+                  prefix=self.target_column,    #your choice
+                  prefix_sep='_',     #your choice
+                  columns=[self.target_column],
+                  dummy_na=self.dummy_na,    #will try to impute later so leave NaNs in place
+                  drop_first=self.drop_first    #really should be True but could screw us up later
+                  )
+    return X_
+    
+  def fit_transform(self, X, y = None):
+    result = self.transform(X)
+    return result
+  
+  
+class DropColumnsTransformer(BaseEstimator, TransformerMixin):
+  def __init__(self, column_list, action='drop'):
+    assert action in ['keep', 'drop'], f'{self.__class__.__name__} action {action} not in ["keep", "drop"]'
+    assert isinstance(column_list, list), f'{self.__class__.__name__} constructor expected list but got {type(column_list)} instead.'
+    self.column_list = column_list
+    self.action = action
+
+  def fit(self, X, y = None):
+    print(f"\nWarning: {self.__class__.__name__}.fit does nothing.\n")
+    return X
+
+  def transform(self, X):
+    assert isinstance(X, pd.core.frame.DataFrame), f'{self.__class__.__name__}.transform expected Dataframe but got {type(X)} instead.'
+    column_list = set(self.column_list)
+    cols = set(X.columns)
+    diff = column_list - cols
+
+    if len(diff) > 0:
+      if self.action == 'drop':
+        print(f"\nWarning: {self.__class__.__name__} does not contain these columns to drop: {diff}.\n")
+      assert self.action != 'keep', f'{self.__class__.__name__} does not contain these columns to keep: {diff}'
+
+    X_ = X.copy()
+    if self.action == 'keep':
+      diff = cols - column_list 
+      X_.drop(diff, errors='ignore', axis=1, inplace=True)
+    else:
+      X_.drop(self.column_list, errors='ignore', axis=1, inplace=True)
+    return X_
+    
+  def fit_transform(self, X, y = None):
+    result = self.transform(X)
+    return result
