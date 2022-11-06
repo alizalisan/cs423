db954f34672dea5d92e170ee5858af744d92f3e8
Aliza Lisan <36656209+alizalisan@users.noreply.github.com>
2022-11-06 00:14:31 -0700

    Update library.py

--- library.py
+++ library.py
@@ -352,16 +352,16 @@ def find_random_state(features_df, labels, n=200):
 
 
 
-# titanic_transformer = Pipeline(steps=[
-#     ('drop', DropColumnsTransformer(['Age', 'Gender', 'Class', 'Joined', 'Married',  'Fare'], 'keep')),
-#     ('gender', MappingTransformer('Gender', {'Male': 0, 'Female': 1})),
-#     ('class', MappingTransformer('Class', {'Crew': 0, 'C3': 1, 'C2': 2, 'C1': 3})),
-#     ('ohe', OHETransformer(target_column='Joined')),
-#     ('age', TukeyTransformer(target_column='Age', fence='outer')), #from chapter 4
-#     ('fare', TukeyTransformer(target_column='Fare', fence='outer')), #from chapter 4
-#     ('minmax', MinMaxTransformer()),  #from chapter 5
-#     ('imputer', KNNTransformer())  #from chapter 6
-#     ], verbose=True)
+titanic_transformer = Pipeline(steps=[
+    ('drop', DropColumnsTransformer(['Age', 'Gender', 'Class', 'Joined', 'Married',  'Fare'], 'keep')),
+    ('gender', MappingTransformer('Gender', {'Male': 0, 'Female': 1})),
+    ('class', MappingTransformer('Class', {'Crew': 0, 'C3': 1, 'C2': 2, 'C1': 3})),
+    ('ohe', OHETransformer(target_column='Joined')),
+    ('age', TukeyTransformer('Age', 'outer')), #from chapter 4
+    ('fare', TukeyTransformer('Fare', 'outer')), #from chapter 4
+    ('minmax', MinMaxTransformer()),  #from chapter 5
+    ('imputer', KNNTransformer())  #from chapter 6
+    ], verbose=True)
 
 
 
